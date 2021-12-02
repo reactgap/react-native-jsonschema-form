e02860589eef8ddf846d1fa2c256e1a864192ed5
mac01 <momi.mac01@gmail.com>
2021-12-02 08:49:36 +0700

    export date picker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import Form from './components/Form';
 
 import PickerOption from './components/widgets/Picker/PickerOption';
+import DatePicker from './components/widgets/DateTime/DatePicker';
+
 import Avatar from './components/widgets/Avatar';
 import CountryDb from './data/vietnam_provinces_districts.json';
 import AnimatedInput from './components/widgets/AnimatedInput';
-export { PickerOption, Avatar, CountryDb, AnimatedInput };
+export { PickerOption, Avatar, CountryDb, AnimatedInput, DatePicker };
 export default Form;
